5228a27b2450e7012dd8c12c0219ae6700d7a739
ginorojo <ginorojoj@gmail.com>
2025-05-08 16:55:44 -0400

    funcion seerie 5

--- gino.js
+++ gino.js
@@ -1,5 +1,18 @@
-export function saludar() {
-    console.log("hola")
+// export function saludar() {
+//     console.log("hola")
+    
+// }
+// saludar()
+
+function seriecinco(numeroVueltas) {
+    let array =[]
+
+    for (let i = 1; i < numeroVueltas; i++){
+        array.push(i*5)
+
+    }
+    return array
     
 }
-saludar()
+
+console.log(seriecinco(5))
