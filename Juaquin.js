dfd4c2696f53724f4b8c95e854eeea867e203b4e
Juaquin Morales <juaquin.moralesbb@gmail.com>
2025-05-08 17:45:57 -0300

    Mayor

--- Juaquin.js
+++ Juaquin.js
@@ -1,5 +1,15 @@
-function darleDeComerAlPerro() {
-    console.log("Le has dado de comer al perro.");
+function encontrarMayor(a, b, c) {
+    let mayor = a;
+  
+    if (b > mayor) {
+      mayor = b;
+    }
+    if (c > mayor) {
+      mayor = c;
+    }
+  
+    return mayor;
   }
   
-  darleDeComerAlPerro();
+  let mayor = encontrarMayor(21, 54, 23);
+  console.log("El número mayor es:", mayor);
