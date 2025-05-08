e897f9b5408135c19352b4eb3dee167ba7b41870
Kevin <kevinmanueldiazluis@gmail.com>
2025-05-08 13:47:48 -0700

    Funcion-sumatoria

--- kevin.js
+++ kevin.js
@@ -1,4 +1,15 @@
 export function edad(edad) {
     console.log("Tu edad es " + edad)
 }
-saludar();
+edad();
+
+export function sumatoria3(numero1, numero2, numero3) {
+    let array = [numero1, numero2, numero3];
+    for (let i = 0; i < array.length; i++) {
+        array[i] = parseFloat(prompt("Dame el numero " + (i + 1)))
+    }
+    let value = array.reduce((aculumador, element) => aculumador + element, 0)
+    alert("Tu resultado es: " + value)
+
+}
+sumatoria3()
