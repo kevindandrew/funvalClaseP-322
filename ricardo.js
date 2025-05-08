045e599e672b945d77b91bdb999925eee4301e38
Ricardo Hernandez <richhquim@gmail.com>
2025-05-08 14:48:02 -0600

    003

--- ricardo.js
+++ ricardo.js
@@ -1,5 +1,18 @@
 function esperar() {
     console.log("Esperando...");
 }
+esperar()
 
 export { esperar };
+
+// funcion que retorne n terminos de los numeros multiplos de 3  
+function multiplosDeTres(n) {
+    let multiplos = [];
+    for (let i = 1; i <= n; i++) {
+        multiplos.push(i * 3);
+
+    }
+    console.log(multiplos);
+}
+
+multiplosDeTres(10);
